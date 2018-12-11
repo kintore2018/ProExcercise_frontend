d4525743ced204b78918680c7d94118e0e26ffc6
“Yuya <yuya.hayashida@bizreach.co.jp>
2018-12-11 12:33:59 +0900

    server.js fix

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 const path = require('path');
 app.get('/*', function(req, res){
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/fitness/index.html'));
 });
 console.log('Server listening on port 8080');
