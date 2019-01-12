804702171ef9a08502f17bd710d854bd81a69c3e
“Yuya <yuya.hayashida@bizreach.co.jp>
2019-01-12 18:22:06 +0900

    add_erroe_handling2

--- src/app/services/trainer-list-api.service.ts
+++ src/app/services/trainer-list-api.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpErrorResponse } from "@angular/common/http"
+import { Observable, throwError } from "rxjs";
 import { Subject } from "rxjs/index"
-import { catchError } from "rxjs/internal/operators";
+import {catchError, retry} from "rxjs/internal/operators";
 import { of } from "rxjs/index"
+import {error} from "util";
 
 @Injectable({
   providedIn: "root"
@@ -16,13 +18,27 @@ export class TrainerListApiService {
   constructor(private http: HttpClient) {}
 
   getTrainers(): Observable<any> {
-    return this.http.get<any>(this.url).pipe(
-      catchError((error) => {
-        console.error('エラー', error);
-        this.loadingError$.next(true);
-        console.log(this.loadingError$);
-        return of();
-      })
-    );
+    return this.http.get<any>(this.url)
+      .pipe(
+        retry(3),
+        catchError(this.handleError)
+      );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      alert('通信エラーが発生しました。ブラウザを再読み込みしてください。'));
+  };
+
 }
