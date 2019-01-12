cf162deb8d5c4091dc38452813bfea593ae05235
“Yuya <yuya.hayashida@bizreach.co.jp>
2019-01-12 17:15:57 +0900

    add_erroe_handling

--- src/app/pages/trainers-page/trainers-page.component.ts
+++ src/app/pages/trainers-page/trainers-page.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import {
-  // TrainerList,
   TrainerListApiService
 } from "../../services/trainer-list-api.service";
-import {Observable} from "rxjs/index";
+import { Observable } from "rxjs/index";
 
 @Component({
   selector: "app-trainers-page",
@@ -23,15 +22,3 @@ export class TrainersPageComponent implements OnInit {
     this.trainers$ = this.trainerListAPI.getTrainers();
   }
 }
-
-//圧倒的な違いは@inputやoutputするときのタイミングで利用する場合はngOninitに書く
-//ここでコンロの火をつけるイメージ
-//subscribeはデータに変化があれば実行する（状態を監視している）
-
-//コンポーネント＝レイヤー
-
-// あとでコンソールで確かめる時
-// .subscribe(heroes => {
-//   console.log(heroes.foo);     // NG
-//   console.log(heroes['foo']);  // OK
-// });
--- src/app/services/trainer-list-api.service.ts
+++ src/app/services/trainer-list-api.service.ts
@@ -1,22 +1,28 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-
-// export interface TrainerList {
-//   name: swwtring;
-//   area: string;
-// }
+import { Subject } from "rxjs/index"
+import { catchError } from "rxjs/internal/operators";
+import { of } from "rxjs/index"
 
 @Injectable({
   providedIn: "root"
 })
 export class TrainerListApiService {
+  loadingError$ = new Subject<boolean>();
   url = "https://api-proex-2018.herokuapp.com/api/trainer/list";
   // url = "assets/mock/sample.json";
 
   constructor(private http: HttpClient) {}
 
   getTrainers(): Observable<any> {
-    return this.http.get<any>(this.url);
+    return this.http.get<any>(this.url).pipe(
+      catchError((error) => {
+        console.error('エラー', error);
+        this.loadingError$.next(true);
+        console.log(this.loadingError$);
+        return of();
+      })
+    );
   }
 }
