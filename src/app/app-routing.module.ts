ff7737950b37de58ab634c4013e5420522c59f02
“Yuya <yuya.hayashida@bizreach.co.jp>
2019-01-26 19:55:30 +0900

    fi_form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,10 +24,6 @@ const routes: Routes = [
     component: PrivacyPolicyPageComponent
   },
   {
-    path: 'request',
-    component: RequestFormPageComponent
-  },
-  {
     path: '**',
     redirectTo: ''
   }
