694f48ec36a308d0ce1a7614a099eea8f8eb6945
masaya48 <msya0408@gmail.com>
2019-05-26 14:17:45 +0900

    refactor: lintエラー解消

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./components/header/header.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
-import { TrainersPageComponent } from "./pages/trainers-page/trainers-page.component";
-import { TrainerDetailPageComponent } from "./pages/trainer-detail-page/trainer-detail-page.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { TrainersPageComponent } from './pages/trainers-page/trainers-page.component';
+import { TrainerDetailPageComponent } from './pages/trainer-detail-page/trainer-detail-page.component';
 import { PrivacyPolicyPageComponent } from './pages/privacy-policy-page/privacy-policy-page.component';
 import { RequestFormPageComponent } from './pages/request-form-page/request-form-page.component';
 
@@ -21,7 +21,7 @@ import { RequestFormPageComponent } from './pages/request-form-page/request-form
     FooterComponent,
     TrainerDetailPageComponent,
     PrivacyPolicyPageComponent,
-    RequestFormPageComponent
+    RequestFormPageComponent,
   ],
   imports: [
     BrowserModule,
